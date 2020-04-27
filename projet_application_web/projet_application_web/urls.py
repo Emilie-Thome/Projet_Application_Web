a6d741e054f61abd31e976c725adc79c2cc04444
Emilie Thomé <emilie.thome@hotmail.fr>
2020-04-27 14:41:39 +0200

    intégration initiale de django.contrib.auth.urls et template pour login

--- projet_application_web/projet_application_web/urls.py
+++ projet_application_web/projet_application_web/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('taskmanager/', include('taskmanager.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
