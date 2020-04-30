f97ac8d189b6ba44d9ff11f1a18732c3d382d8b7
Emilie Thomé <emilie.thome@hotmail.fr>
2020-04-30 10:33:37 +0200

    -ajout des models Status et Task

--- projet_application_web/taskmanager/models.py
+++ projet_application_web/taskmanager/models.py
@@ -5,6 +5,21 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=200)
     members = models.ManyToManyField(User)
-    # TODO
     def __str__(self):
         return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    assignee = models.ForeignKey(User)
+    start_date = models.DateField()
+    due_date = models.DateField()
+    priority = models.IntegerField()
+    status = models.ForeignKey('Status', on_delete=models.CASCADE) # TODO it's not relevant to delete
+                                                                    # a task because the status is deleted
