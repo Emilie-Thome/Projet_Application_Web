0438ea1848b37ff18f530c727fe1c6929d81749b
Emilie Thomé <emilie.thome@hotmail.fr>
2020-04-30 09:46:47 +0200

    -ajout project à l'administration -vue affichage projets -template affichage projets

--- projet_application_web/taskmanager/views.py
+++ projet_application_web/taskmanager/views.py
@@ -1,13 +1,10 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-# from taskmanager.models import ...
+from taskmanager.models import Project
 
 @login_required(login_url='/accounts/login/')
 def projects(request):
-    """ View projects """
-    return render(request, 'taskmanager/projects.html',)
-
-
-def logout_view(request):
-    logout(request)
+    user = request.user
+    projects = Project.objects.filter(members__in = [user])
+    return render(request, 'taskmanager/projects.html',{'projects': projects})
