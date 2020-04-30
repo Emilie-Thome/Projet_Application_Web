e874fb38f1d9fd4a3e1884217bd38957a4495f77
Emilie Thomé <emilie.thome@hotmail.fr>
2020-04-30 16:33:57 +0200

    -ajout de Journal dans models.py et en administration

--- projet_application_web/taskmanager/admin.py
+++ projet_application_web/taskmanager/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Status, Task
+from .models import Project, Status, Task, Journal
 
 admin.site.register(Project)
 admin.site.register(Status)
 admin.site.register(Task)
+admin.site.register(Journal)
--- projet_application_web/taskmanager/models.py
+++ projet_application_web/taskmanager/models.py
@@ -23,3 +23,8 @@ class Task(models.Model):
     priority = models.IntegerField()
     status = models.ForeignKey('Status', on_delete=models.CASCADE) # TODO it's not relevant to delete
                                                                     # a task because the status is deleted
+class Journal(models.Model):
+    date = models.DateField()
+    entry = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #TODO a default user that signify that the user is deleted
+    task = models.ForeignKey('Task', on_delete=models.CASCADE)
