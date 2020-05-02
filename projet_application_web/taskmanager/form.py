47246cb7a85fb5e3f600d026a1a640510627b77c
Emilie Thomé <emilie.thome@hotmail.fr>
2020-05-02 09:40:52 +0200

    -TaskForm : check if the assignee is part of project.members.all()

--- projet_application_web/taskmanager/form.py
+++ projet_application_web/taskmanager/form.py
@@ -5,3 +5,15 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(TaskForm, self).clean()
+        project = cleaned_data.get('project')
+        assignee = cleaned_data.get('assignee')
+
+        if not(assignee in list(project.members.all())) :
+            raise forms.ValidationError(
+                "The assignee must be a project member !"
+            )
+
+        return cleaned_data
