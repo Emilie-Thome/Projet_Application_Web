5779710ac8dc01398efd1727bb3974e5f6e4e634
Emilie Thomé <emilie.thome@hotmail.fr>
2020-05-03 17:41:06 +0200

    -template update

--- projet_application_web/taskmanager/models.py
+++ projet_application_web/taskmanager/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,6 +6,9 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=200)
     members = models.ManyToManyField(User)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -24,18 +28,24 @@ class Task(models.Model):
     assignee = models.ForeignKey(User, on_delete=models.CASCADE) # TODO, limit_choices_to=project.members)
     start_date = models.DateField()
     due_date = models.DateField()
-    priority = models.IntegerField()
+    priority = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
     status = models.ForeignKey('Status', on_delete=models.CASCADE) # TODO it's not relevant to delete
                                                                     # a task because the status is deleted
+    class Meta:
+        ordering = ['priority']
+
     def __str__(self):
         return self.name
 
 
 class Journal(models.Model):
-    date = models.DateField()
+    date = models.DateTimeField()
     entry = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE) #TODO a default user that signify that the user is deleted
     task = models.ForeignKey('Task', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['date']
 
     def __str__(self):
         return self.entry
