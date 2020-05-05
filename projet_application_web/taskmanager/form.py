c91e77b2094682a677e6cfb57f420670df800eca
Emilie Thomé <emilie.thome@hotmail.fr>
2020-05-05 15:34:25 +0200

    form editted to be more concise

--- projet_application_web/taskmanager/form.py
+++ projet_application_web/taskmanager/form.py
@@ -6,14 +6,32 @@ class TaskForm(forms.ModelForm):
         model = Task
         exclude = ('project',)
 
-        widgets = {'name':forms.TextInput(attrs={'class':'form-control col-sm-10', 'placeholder':'Name...'}),
-                   'description':forms.Textarea(attrs={'class':'form-control col-sm-10', 'placeholder':'Description...'}),
-                   'assignee':forms.Select(attrs={'class':'form-control col-sm-10', 'placeholder':'---------'}),
-                   'start_date':forms.DateInput(attrs={'class':'form-control col-sm-10'}),
-                   'due_date':forms.DateInput(attrs={'class':'form-control col-sm-10'}),
-                   'priority':forms.NumberInput(attrs={'min':1,'max':5,'class':'form-control col-sm-10'}),
-                   'status':forms.Select(attrs={'class':'form-control col-sm-10', 'placeholder':'---------'})
+        widgets = {'name':forms.TextInput(attrs={'class':'form-control col-sm-8', 'placeholder':'Name...'}),
+                   'description':forms.Textarea(attrs={'class':'form-control col-sm-8 h-25', 'rows':'2','style':'height:30%', 'placeholder':'Description...'}),
+                   'assignee':forms.Select(attrs={'class':'form-control col-sm-8', 'placeholder':'---------'}),
+                   'start_date':forms.DateInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
+                   'due_date':forms.DateInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
+                   'priority':forms.NumberInput(attrs={'min':1,'max':5,'class':'form-control col-sm-8'}),
+                   'status':forms.Select(attrs={'class':'form-control col-sm-8', 'placeholder':'---------'})
                    }
+
+        labels = {
+            'name': 'Title :',
+            'description': 'Decription :',
+            'assignee': 'Assignee :',
+            'start_date': 'Start date :',
+            'due_date': 'Due date :',
+            'priority': 'Priority :',
+            'status':  'Status :',
+        }
+        help_texts = {
+            'name': 'Some useful help text.',
+        }
+        error_messages = {
+            'name': {
+                'max_length': "This writer's name is too long.",
+            },
+        }
 
 
 
