476eb01582b0771c85f64ae5903c0307d1c1c655
Emilie Thomé <emilie.thome@hotmail.fr>
2020-05-05 16:35:06 +0200

    error message added for due date and start date went it is wrong

--- projet_application_web/taskmanager/form.py
+++ projet_application_web/taskmanager/form.py
@@ -9,8 +9,8 @@ class TaskForm(forms.ModelForm):
         widgets = {'name':forms.TextInput(attrs={'class':'form-control col-sm-8', 'placeholder':'Name...'}),
                    'description':forms.Textarea(attrs={'class':'form-control col-sm-8 h-25', 'rows':'2','style':'height:30%', 'placeholder':'Description...'}),
                    'assignee':forms.Select(attrs={'class':'form-control col-sm-8', 'placeholder':'---------'}),
-                   'start_date':forms.DateInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
-                   'due_date':forms.DateInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
+                   'start_date':forms.TextInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
+                   'due_date':forms.TextInput(attrs={'class':'form-control col-sm-8', 'type':'date'}),
                    'priority':forms.NumberInput(attrs={'min':1,'max':5,'class':'form-control col-sm-8'}),
                    'status':forms.Select(attrs={'class':'form-control col-sm-8', 'placeholder':'---------'})
                    }
@@ -24,26 +24,30 @@ class TaskForm(forms.ModelForm):
             'priority': 'Priority :',
             'status':  'Status :',
         }
-        help_texts = {
-            'name': 'Some useful help text.',
-        }
-        error_messages = {
-            'name': {
-                'max_length': "This writer's name is too long.",
-            },
-        }
 
 
-
+    error_css_class = "error"
     def clean(self):
         cleaned_data = super(TaskForm, self).clean()
+
+        ''' First, Assignee must be a project member.
+            This error can only be raised in the admin page '''
         project = cleaned_data.get('project')
         assignee = cleaned_data.get('assignee')
 
         if project :
             if not(assignee in list(project.members.all())) :
-                raise forms.ValidationError(
-                    "The assignee must be a project member !"
-                )
+                self.add_error("assignee",
+                               "The assignee must be a project member !"
+                               )
+
+        ''' Second, Due date must be after Start date '''
+        start_date = cleaned_data.get('start_date')
+        due_date = cleaned_data.get('due_date')
+
+        if due_date < start_date :
+            self.add_error("due_date",
+                           "The due date must be after the start date !"
+                           )
 
         return cleaned_data
