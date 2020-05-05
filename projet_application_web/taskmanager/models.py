cbad18ec9d3c9e836233197d02e109c5f637d03a
Emilie Thomé <emilie.thome@hotmail.fr>
2020-05-05 14:44:48 +0200

    verbose name added and date automated for journal (as it is a post)

--- projet_application_web/taskmanager/models.py
+++ projet_application_web/taskmanager/models.py
@@ -1,13 +1,15 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=80)
-    members = models.ManyToManyField(User)
+    name = models.CharField(max_length=80, verbose_name="Project name")
+    members = models.ManyToManyField(User, verbose_name="Project members")
 
     class Meta:
+        verbose_name = "project"
         ordering = ['name']
 
     def __str__(self):
@@ -15,23 +17,27 @@ class Project(models.Model):
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, verbose_name="Status name")
+
+    class Meta:
+        verbose_name = "status"
 
     def __str__(self):
         return self.name
 
 
 class Task(models.Model):
-    project = models.ForeignKey('Project', on_delete=models.CASCADE)
-    name = models.CharField(max_length=80)
-    description = models.TextField(max_length=200)
-    assignee = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    due_date = models.DateField()
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, verbose_name="Task project")
+    name = models.CharField(max_length=80, verbose_name="Task name")
+    description = models.TextField(max_length=200, verbose_name="Task description")
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Task assignee")
+    start_date = models.DateField(verbose_name="Task start date")
+    due_date = models.DateField(verbose_name="Task due date")
     priority = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    status = models.ForeignKey('Status', on_delete=models.CASCADE) # TODO it's not relevant to delete
+    status = models.ForeignKey('Status', on_delete=models.CASCADE, verbose_name="Task status") # TODO it's not relevant to delete
                                                                     # a task because the status is deleted
     class Meta:
+        verbose_name = "task"
         ordering = ['priority']
 
     def __str__(self):
@@ -39,12 +45,13 @@ class Task(models.Model):
 
 
 class Journal(models.Model):
-    date = models.DateTimeField()
-    entry = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #TODO a default user that signify that the user is deleted
-    task = models.ForeignKey('Task', on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Post date")
+    entry = models.CharField(max_length=200, verbose_name="Journal entry")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Journal author") #TODO a default user that signify that the user is deleted
+    task = models.ForeignKey('Task', on_delete=models.CASCADE, verbose_name="Journal task")
 
     class Meta:
+        verbose_name = "journal"
         ordering = ['date']
 
     def __str__(self):
